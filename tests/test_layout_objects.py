93a3d898f0faa027cf1461ea1dc7735ddc4897cc
kosdmit <kosdmit@hotmail.com>
2024-07-21 20:33:18 +0300

    Add missed css_class to accordion template

--- tests/test_layout_objects.py
+++ tests/test_layout_objects.py
@@ -247,6 +247,22 @@ class TestBootstrapLayoutObjects:
         assert html.count('name="password1"') == 1
         assert html.count('name="password2"') == 1
 
+    def test_accordion_css_class_is_applied(self):
+        classes = "one two three"
+        test_form = SampleForm()
+        test_form.helper = FormHelper()
+        test_form.helper.form_tag = False
+        test_form.helper.layout = Layout(
+            Accordion(
+                AccordionGroup("one", "first_name"),
+                css_class=classes,
+                css_id="super-accordion",
+            )
+        )
+        html = render_crispy_form(test_form)
+
+        assert html.count('<div class="%s" id="super-accordion"' % classes) == 1
+
     def test_accordion_active_false_not_rendered(self):
         test_form = SampleForm()
         test_form.helper = FormHelper()
